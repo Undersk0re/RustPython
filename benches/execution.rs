574a973e8b5b9ef2a2b1936b989d65c28366e690
Simone_Pellacani <automatic.alert.log@gmail.com>
2025-05-03 15:32:53 +0200

    DARK_BLUE param in plot/gnpplot ndidn't change

--- benches/execution.rs
+++ benches/execution.rs
@@ -1,181 +1,124 @@
-use criterion::measurement::WallTime;
 use criterion::{
-    Bencher, BenchmarkGroup, BenchmarkId, Criterion, Throughput, black_box, criterion_group,
-    criterion_main,
+    BenchmarkId, Criterion, Throughput, black_box, criterion_group,
+    criterion_main, PlotConfiguration, AxisScale
 };
-use rustpython_compiler::Mode;
-use rustpython_vm::{Interpreter, PyResult, Settings};
+
+
+use::criterion::BenchmarkGroup
+
+
 use std::collections::HashMap;
 use std::path::Path;
+use pyo3::types::PyAnyMethods;
+
+// Constants for benchmark modes
+const MODE_CPYTHON: &str = "cpython";
+const MODE_RUSTPYTHON: &str = "rustpython";
+const PATH_OUT_RESULT_BENCH:&str = "./benches/benchmarks/";
+const TEST_NAMES: [&str; 3] = [
+    "parse_to_ast",
+    "pystone",
+    "execution"
+];
 
 
-/// Benchmark a Python code snippet using CPython (via PyO3)
-fn bench_cpython_code(b: &mut Bencher, source: &str) {
-    // Convert the Rust string to a C-compatible string for PyO3
-    let c_str_source_head = std::ffi::CString::new(source).unwrap();
-    let c_str_source = c_str_source_head.as_c_str();
-    // Acquire the Python GIL (Global Interpreter Lock) and run the code
-    pyo3::Python::with_gil(|py| {
-        b.iter(|| {
-            // Compile and execute the Python code as a module
-            let module = pyo3::types::PyModule::from_code(py, c_str_source, c"", c"")
-                .expect("Error running source");
-            // Use black_box to prevent compiler optimizations
-            black_box(module);
-        })
-    })
+
+struct BenchmarkLocalGroup {
+    benches: HashMap<String, String>,
+    mode: &'static str,
 }
 
-/// Benchmark a Python code snippet using RustPython
-fn bench_rustpython_code(b: &mut Bencher, name: &str, source: &str) {
-    // NOTE: Take long time.
-    // Set up RustPython interpreter settings
-    let mut settings = Settings::default();
-    settings.path_list.push("Lib/".to_string());
-    settings.write_bytecode = false;
-    settings.user_site_directory = false;
-    // Create an interpreter without the standard library
-    Interpreter::without_stdlib(settings).enter(|vm| {
-        // Note: bench_cpython is both compiling and executing the code.
-        // As such we compile the code in the benchmark loop as well.
-        // For each iteration, compile and execute the code
-        b.iter(|| {
-            // Compile the Python code
-            let code = vm.compile(source, Mode::Exec, name.to_owned()).unwrap();
-            // Create a new scope (namespace) with built-in functions
-            let scope = vm.new_scope_with_builtins();
-            // Run the compiled code in the new scope
-            let res: PyResult = vm.run_code_obj(code.clone(), scope);
-            // Unwrap the result, panicking if there was an error
-            vm.unwrap_pyresult(res);
-        })
-    })
-}
+impl BenchmarkLocalGroup {
+    fn new(benchmark_dir: &Path, mode: &'static str ) -> Self {
+        let benches = benchmark_dir
+            .read_dir()
+            .unwrap()
+            .map(|entry| {
+                let path = entry.unwrap().path();
+                (
+                    path.file_name().unwrap().to_str().unwrap().to_owned(),
+                    std::fs::read_to_string(path).unwrap(),
+                )
+            })
+            .collect::<HashMap<_, _>>();
 
-
-
-/// Main function that sets up and runs all benchmarks
-fn criterion_benchmark(c: &mut Criterion, mode:char) {
-    // mode c== cpython, mode r== rustpython
-    // Path to the directory containing benchmark files
-    let benchmark_dir = Path::new("./benches/benchmarks/");
-    // Read all files in the directory into a HashMap (filename -> file contents)
-    let mut benches = benchmark_dir
-        .read_dir()
-        .unwrap()
-        .map(|entry| {
-            let path = entry.unwrap().path();
-            (
-                path.file_name().unwrap().to_str().unwrap().to_owned(),
-                std::fs::read_to_string(path).unwrap(),
-            )
-        })
-        .collect::<HashMap<_, _>>();
-
-
-    /********************************************
-    *           benchmark_file_parsing
-    *********************************************/
-    // Benchmark parsing for each file
-    let mut parse_group = c.benchmark_group("parse_to_ast");
-    for (name, contents) in &benches {
-
-        // Set the throughput (for reporting) to the number of bytes in the file
-        parse_group.throughput(Throughput::Bytes(contents.len() as u64));
-        // Benchmark parsing with RustPython's parser
-        if mode=='r' {
-            parse_group.bench_function(BenchmarkId::new("rustpython", name), |b| {
-                b.iter(|| ruff_python_parser::parse_module(contents).unwrap())
-            });
+        BenchmarkLocalGroup {
+            benches,
+            mode,
         }
-        if mode=='c'{
-            // Benchmark parsing with CPython's parser (via PyO3)
-            parse_group.bench_function(BenchmarkId::new("cpython", name), |b| {
-                use pyo3::types::PyAnyMethods;
-                pyo3::Python::with_gil(|py| {
-                    let builtins =
-                        pyo3::types::PyModule::import(py, "builtins").expect("Failed to import builtins");
-                    let compile = builtins.getattr("compile").expect("no compile in builtins");
-                    b.iter(|| {
-                        let x = compile
-                            .call1((contents, name, "exec"))
-                            .expect("Failed to parse code");
-                        black_box(x);
-                    })
-                })
-            });
-        }
-
     }
-    parse_group.finish();
 
 
-    /********************************************
-    *           benchmark_pystone
-    *********************************************/
-
-    // If there is a PyStone benchmark, run it separately
-    if let Some(pystone_contents) = benches.remove("pystone.py") {
-        let mut pystone_group = c.benchmark_group("pystone");
-        //benchmark_pystone(&mut pystone_group, pystone_contents);
-        for idx in (10_000..=30_000).step_by(10_000) {
-            // Insert the number of loops into the code
-            let code_with_loops = format!("LOOPS = {}\n{}", idx, pystone_contents);
-            let code_str = code_with_loops.as_str();
-    
-            // Set throughput to the number of loops
-            pystone_group.throughput(Throughput::Elements(idx as u64));
-            // Benchmark with CPython
-            if mode=='c'{
-                pystone_group.bench_function(BenchmarkId::new("cpython", idx), |b| {
-                    bench_cpython_code(b, code_str)
-                });
-
+    fn run_parse_benchmarks(&self, c: &mut Criterion) {
+        let mut parse_group = c.benchmark_group(TEST_NAMES[0]);
+        
+        parse_group.plot_config(PlotConfiguration::default()
+            .summary_scale(AxisScale::Logarithmic) );
+            
+        for (name, contents) in &self.benches {
+            parse_group.throughput(Throughput::Bytes(contents.len() as u64));
+            
+            if self.mode == MODE_RUSTPYTHON {
+                parse_group.bench_with_input(
+                    BenchmarkId::new(MODE_RUSTPYTHON, name),
+                    contents,
+                    |b, contents| {
+                        b.iter(|| ruff_python_parser::parse_module(contents).unwrap())
+                    }
+                );
             }
-            if mode=='r'{
-                // Benchmark with RustPython
-                pystone_group.bench_function(BenchmarkId::new("rustpython", idx), |b| {
-                    bench_rustpython_code(b, "pystone", code_str)
-                });
-
+            
+            if self.mode == MODE_CPYTHON {
+                parse_group.bench_with_input(
+                    BenchmarkId::new(MODE_CPYTHON, name),
+                    contents,
+                    |b, contents| {
+                        pyo3::Python::with_gil(|py| {
+                            let builtins = pyo3::types::PyModule::import(py, "builtins")
+                                .expect("Failed to import builtins");
+                            let compile = builtins.getattr("compile")
+                                .expect("no compile in builtins");
+                            b.iter(|| {
+                                let x = compile
+                                    .call1((contents, name, "exec"))
+                                    .expect("Failed to parse code");
+                                black_box(x);
+                            })
+                        })
+                    }
+                );
             }
         }
-        pystone_group.finish();
+        parse_group.finish();
     }
 
-    /********************************************
-    *           benchmark_execution
-    *********************************************/
-    // Benchmark execution for each file
-    let mut execution_group = c.benchmark_group("execution");
-    for (name, contents) in &benches {
-        // Register the CPython benchmark
-        if mode == 'c' {
-            execution_group.bench_function(BenchmarkId::new("cpython", name), |b| {
-                bench_cpython_code(b, contents)
-            });
 
-        }
-        // Register the RustPython benchmark
-        if mode=='r'{
-            execution_group.bench_function(BenchmarkId::new("rustpython", name), |b| {
-                bench_rustpython_code(b, name, contents)
-            });
-        }
+
+    fn run_all_benchmarks(&self, c: &mut Criterion) {
+        self.run_parse_benchmarks(c);
     }
-    execution_group.finish();
+}
 
+fn criterion_benchmark_c(c: &mut Criterion) {
+    let benchmark_c = BenchmarkLocalGroup::new(Path::new(PATH_OUT_RESULT_BENCH), MODE_CPYTHON);
+    benchmark_c.run_all_benchmarks(c);
+}
+
+fn criterion_benchmark_r(c: &mut Criterion) {
+    let benchmark_r = BenchmarkLocalGroup::new(Path::new(PATH_OUT_RESULT_BENCH), MODE_RUSTPYTHON);
+    benchmark_r.run_all_benchmarks(c);
 }
 
 
-pub fn criterion_benchmark_cpython(c: &mut Criterion,){
-    criterion_benchmark(c,'c');
-}
 
-pub fn criterion_benchmark_rustpython(c: &mut Criterion,){
-    criterion_benchmark(c,'r');
-}
 
-// Register the benchmarks with Criterion
-criterion_group!(benches, criterion_benchmark_cpython,criterion_benchmark_rustpython);
+criterion_group!(
+    name = benches;
+    config = Criterion::default();
+    targets = criterion_benchmark_c,criterion_benchmark_r
+);
+
+
+
+
 criterion_main!(benches);
